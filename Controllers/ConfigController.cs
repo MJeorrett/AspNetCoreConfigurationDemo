d89468fd417f60adcaf9ac0195be627c546dcab6
MJeorrett <matthew@clearskylogic.com>
2021-03-13 21:58:47 +0000

    add GetAll and GetProvider endpoints

--- Controllers/ConfigController.cs
+++ Controllers/ConfigController.cs
@@ -1,29 +1,74 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace AspNetCoreConfigurationDemo.Controllers
 {
-    [Route("api/config")]
     [ApiController]
     public class ConfigController : ControllerBase
     {
         private readonly IConfiguration _config;
+        private readonly IConfigurationRoot _configRoot;
 
         public ConfigController(IConfiguration config)
         {
             _config = config;
+            _configRoot = (IConfigurationRoot)config;
         }
 
-        [HttpGet("providers")]
+        [HttpGet("api/config-providers")]
         public ActionResult GetProviders()
         {
             string result = "";
-            var configRoot = (IConfigurationRoot)_config;
 
-            foreach (var provider in (configRoot.Providers.ToList()))
+            foreach (var provider in (_configRoot.Providers.ToList()))
             {
                 result += provider.ToString() + "\n";
+            }
+
+            return Ok(result);
+        }
+
+        [HttpGet("api/config")]
+        public ActionResult GetAll()
+        {
+            var result = "";
+
+            foreach (var configKvp in _config.AsEnumerable())
+            {
+                result += $"{configKvp.Key} => '{configKvp.Value}'\n";
+            }
+
+            return Ok(result);
+        }
+
+        [HttpGet("api/config-providers/{index}")]
+        public ActionResult GetProvider(int index)
+        {
+            if (index >= _configRoot.Providers.Count())
+            {
+                return BadRequest(new
+                {
+                    message = "Index out of range.",
+                });
+            }
+
+            var provider = _configRoot.Providers.ElementAt(index);
+            var providerConfig = new ConfigurationRoot(new List<IConfigurationProvider> { provider })
+                .AsEnumerable();
+
+            var result =
+                $"Name\n" +
+                $"====\n" +
+                $"{provider}\n" +
+                $"\n" +
+                $"Values\n" +
+                $"======\n";
+
+            foreach (var configKvp in providerConfig.AsEnumerable())
+            {
+                result += $"{configKvp.Key} => '{configKvp.Value}'\n";
             }
 
             return Ok(result);
