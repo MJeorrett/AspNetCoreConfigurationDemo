70d2dac497940bb22af792bf66541e636b86c53f
MJeorrett <matthew@clearskylogic.com>
2021-03-13 16:55:35 +0000

    add cofiguration providers endpoint

---
+++ Controllers/ConfigController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Linq;
+
+namespace AspNetCoreConfigurationDemo.Controllers
+{
+    [Route("api/config")]
+    [ApiController]
+    public class ConfigController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public ConfigController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet("providers")]
+        public ActionResult GetProviders()
+        {
+            string result = "";
+            var configRoot = (IConfigurationRoot)_config;
+
+            foreach (var provider in (configRoot.Providers.ToList()))
+            {
+                result += provider.ToString() + "\n";
+            }
+
+            return Ok(result);
+        }
+    }
+}
